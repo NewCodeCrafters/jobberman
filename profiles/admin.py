eb1f41519b2249b948ef01cbfecfb4f3059c9a07
Infinitykay <bekay.asa@icloud.com>
2024-01-31 16:22:37 +0100

    update on profile.model

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
-from .models import WorkExperience, Education, Profiles
-
-@admin.register(WorkExperience)
-class WorkExperienceAdmin(admin.ModelAdmin):
-    list_display = ('user', 'job_title', 'company', 'start_date', 'end_date')
-    search_fields = ('user', 'job_title', 'company', 'description')
-
-@admin.register(Education)
-class EducationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'degree', 'course', 'school', 'graduation_date')
-    search_fields = ('user','degree', 'course', 'school')
+from .models import  Profiles
 
 @admin.register(Profiles)
 class ProfilesAdmin(admin.ModelAdmin):
-    list_display = ('user', 'job_title', 'location', 'contact_email', 'contact_phone')
-    search_fields = ('user__first_name', 'user__last_name', 'job_title', 'location')
+    list_display = ('user', 'location', 'contact_email', 'contact_phone')
+    search_fields = ('user__first_name', 'user__last_name', 'location')
+
+
+# @admin.register(WorkExperience)
+# class WorkExperienceAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'job_title', 'company', 'start_date', 'end_date')
+#     search_fields = ('user', 'job_title', 'company', 'description')
+
+# @admin.register(Education)
+# class EducationAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'degree', 'course', 'school', 'graduation_date')
+#     search_fields = ('user','degree', 'course', 'school')
+
--- profiles/models.py
+++ profiles/models.py
@@ -3,48 +3,27 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class WorkExperience(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='workExperiences')
-    job_title = models.CharField(max_length=255)
-    company = models.CharField(max_length=255)
-    location = models.CharField(max_length=255, blank=True)
-    start_date = models.DateField()
-    currently_working = models.BooleanField(default=True, blank=True)
-    end_date = models.DateField(blank=True, null=True)
-    description = models.TextField(blank=True)
+class PurposeChoice(models.TextChoices):
+    EMPLOYER = "EMPLOYER"
+    CANDIDATES = "CANDIDATES"
+    FREELANCERS = "FREELANCERS"
+    
+  
 
-    def __str__(self):
-        return f"{self.job_title} at {self.company}"
-
-    class Meta:
-        verbose_name = "Work Experience"
-        verbose_name_plural = "Work Experiences"
-
-class Education(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='educations')
-    degree = models.CharField(max_length=255)
-    course = models.CharField(max_length=255)
-    school = models.CharField(max_length=255)
-    graduation_date = models.DateField(blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.degree}, {self.course} at {self.school}"
-
-    class Meta:
-        verbose_name = "Education"
-        verbose_name_plural = "Education"
 
 class Profiles(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     profile_picture = models.ImageField(upload_to='media/', blank=True, null=True)
     location = models.CharField(max_length=255, blank=True)
-    job_title = models.CharField(max_length=255)
+    purpose = models.CharField(
+        max_length=14, choices=PurposeChoice.choices, default=PurposeChoice.CANDIDATES
+    )
     contact_email = models.EmailField(blank=True)
     contact_phone = models.CharField(max_length=20, blank=True)
-    summary = models.TextField(blank=True)
+    # summary = models.TextField(blank=True)
 
-    work_experiences = models.ManyToManyField(WorkExperience, blank=True)
-    educations = models.ManyToManyField(Education, blank=True)
+    # work_experiences = models.ManyToManyField(WorkExperience, blank=True)
+    # educations = models.ManyToManyField(Education, blank=True)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}'s Profile"
@@ -52,3 +31,36 @@ class Profiles(models.Model):
     class Meta:
         verbose_name = "Profile"
         verbose_name_plural = "Profiles"
+        
+        
+# class WorkExperience(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='workExperiences')
+#     job_title = models.CharField(max_length=255)
+#     company = models.CharField(max_length=255)
+#     location = models.CharField(max_length=255, blank=True)
+#     start_date = models.DateField()
+#     currently_working = models.BooleanField(default=True, blank=True)
+#     end_date = models.DateField(blank=True, null=True)
+#     description = models.TextField(blank=True)
+
+#     def __str__(self):
+#         return f"{self.user} {self.job_title} at {self.company}"
+
+#     class Meta:
+#         verbose_name = "Work Experience"
+#         verbose_name_plural = "Work Experiences"
+
+# class Education(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='educations')
+#     degree = models.CharField(max_length=255)
+#     course = models.CharField(max_length=255)
+#     school = models.CharField(max_length=255)
+#     graduation_date = models.DateField(blank=True, null=True)
+
+#     def __str__(self):
+#         return f"{self.user.first_name}, {self.user.last_name} {self.degree}, {self.course} at {self.school}"
+
+#     class Meta:
+#         verbose_name = "Education"
+#         verbose_name_plural = "Education"
+
