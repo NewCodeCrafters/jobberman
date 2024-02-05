c3a6be3b514610b908eb2db8e2f505e4aec79b15
officialgeekcoder <abubasadiq@gmail.com>
2024-02-05 13:04:10 +0100

    Work done sucessfully

--- job/models.py
+++ job/models.py
@@ -9,9 +9,16 @@ class WorkType(models.TextChoices):
 class JobType(models.TextChoices):
     fulltime = "Fulltime"
     contract = "Contract"
+    intenship = "intenship"
+    parttime = "partime"
+
+
 
 class Skill(models.Model):
-    title = models.CharField(max_length=100) # verbose
+    title = models.CharField(max_length=100, verbose_name = "Skills") # verbose
+
+    def __str__(self) -> str:
+        return self.title 
 
 # str
 
@@ -22,4 +29,7 @@ class Job(models.Model):
     job_type = models.CharField(max_length=20, choices=JobType.choices)
     description = models.TextField(help_text="Company Description, Role Description, Qualifications")
     skills = models.ManyToManyField(Skill)
+    
+    def __str__(self) -> str:
+        return self.title 
 
